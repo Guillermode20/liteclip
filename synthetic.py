cce6320ae4d1c2002bcb99cb1883c1c81854b9ab
Guillermode20 <william_hick@hotmail.co.uk>
2026-03-03 16:59:12 +0000

    chore: remove unused synthetic.py API quota check script

--- synthetic.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json  # Import this
-
-headers = {"Authorization": "Bearer syn_395b18c19c32f76e4545fd61ca6e3510"}
-response = requests.get("https://api.synthetic.new/v2/quotas", headers=headers)
-
-# Use json.dumps to format the dictionary
-print(json.dumps(response.json(), indent=4))
