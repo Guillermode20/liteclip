f505733331bcbe71dc81823a458c2c54d2b86dc4
Guillermode20 <william_hick@hotmail.co.uk>
2026-03-10 13:04:42 +0000

    added new icon

--- build.rs
+++ build.rs
@@ -57,7 +57,7 @@ fn main() {
         println!("cargo:rustc-link-lib=gdi32");
 
         let mut res = winres::WindowsResource::new();
-        res.set_icon("app.ico");
+        res.set_icon("logo.ico");
         res.compile().unwrap();
     }
 
